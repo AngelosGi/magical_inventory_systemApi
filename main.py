c7582b8c931b3948cebef88eb0d0e490d3778e25
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-05-26 16:27:48 +0300

    add FastAppi init and prefix "/items"

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi import FastAPI
+from controllers.magic_item_controller import router
+
+app = FastAPI()
+app.include_router(router, prefix="/items")
+
+
 def main():
-    if __name__ == '__main__':
-        main()
+
+
+if __name__ == '__main__':
+    main()
