8c08fd71d53b8d5bac4c7569fe3fd0e9bcfb7a42
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-05-28 19:51:01 +0300

    beautify

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
     DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
