e84befb508e4ad847527f289844d83e4b7a150bf
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-05-28 20:20:21 +0300

    Add database schema

---
+++ create_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    level INT,
+    type VARCHAR(255),
+    rarity_value FLOAT NOT NULL,
+    weight FLOAT,
+    value INT
+);
