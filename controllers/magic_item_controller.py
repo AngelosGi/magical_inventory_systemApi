c541a256df6435eef3fbd14622734ebb1101b04c
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-04 18:59:19 +0300

    add "/" and "/{item_id}" router, refactor naming and docstrings

--- controllers/magic_item_controller.py
+++ controllers/magic_item_controller.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services.magic_item_service import MagicItemService
 from domain.magic_item import CreateItemRequest, MagicItemRead
 from typing import List
@@ -7,23 +7,37 @@ from typing import List
 router = APIRouter()
 
 
-# @router.get('/')
-# async def index():
-#     return {"message": "Welcome to my Magic Items inventory! "}
+@router.get("/")
+async def root():
+    """
+    Welcome message.
+    """
+    return {"message": "Welcome to my Inventory, all things magical!"}
+
 
 @router.get("/all", response_model=List[MagicItemRead])
 async def get_all_items():
     """
-    Fetch all magical items.
+    Fetch all magic items.
     """
     all_items = MagicItemService.get_all_items()
     return all_items
 
 
+@router.get("/{item_id}", response_model=MagicItemRead)
+async def get_item_by_id(item_id: int):
+    """
+    Fetch a magic item by its ID.
+    """
+    item = MagicItemService.get_item_by_id(item_id)
+    if item:
+        return item
+
+
 @router.post("/create")
 async def create_item(item_data: CreateItemRequest):
     """
-    Create a new magical item with random values for weight, durability, and rarity.
+    Create a new magic item with random values for weight, durability, and rarity.
     """
     # Convert Pydantic model to dictionary
     item_data_dict = item_data.dict()
