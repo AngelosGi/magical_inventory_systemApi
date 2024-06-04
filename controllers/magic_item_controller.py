cb782762afa1a1349a4ec436ecf856eb44d3d2c3
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-04 16:07:47 +0300

    add create endpoint plus renaming for better clarity

--- controllers/magic_item_controller.py
+++ controllers/magic_item_controller.py
@@ -1,17 +1,26 @@
 from pydantic import BaseModel
 from fastapi import APIRouter
 from services.magic_item_service import MagicItemService
-from domain.magic_item import CreateItemRequest
+from domain.magic_item import CreateItemRequest, MagicItemRead
+from typing import List
 
 router = APIRouter()
 
 
-# @router.get('/')  # getAll
+# @router.get('/')
 # async def index():
 #     return {"message": "Welcome to my Magic Items inventory! "}
 
+@router.get("/all", response_model=List[MagicItemRead])
+async def get_all_items():
+    """
+    Fetch all magical items.
+    """
+    all_items = MagicItemService.get_all_items()
+    return all_items
 
-@router.post("/create_item")
+
+@router.post("/create")
 async def create_item(item_data: CreateItemRequest):
     """
     Create a new magical item with random values for weight, durability, and rarity.
@@ -22,5 +31,3 @@ async def create_item(item_data: CreateItemRequest):
     # Call the service to generate random values
     created_item = MagicItemService.generate_random_values(item_data_dict)
     return created_item
-
-# change random_item naming
