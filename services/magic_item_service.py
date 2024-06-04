54a8c621d85f8fe7e6a18250acce2e10d7e37744
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-04 16:10:30 +0300

    add get_all_items service

--- services/magic_item_service.py
+++ services/magic_item_service.py
@@ -1,10 +1,13 @@
+from typing import List, Dict, Any
+
 from repositories.magic_item_repository import MagicItemRepository
 import random
 
 
 class MagicItemService:
+
     @staticmethod
-    def create_random_item(item_data: dict) -> dict:
+    def generate_random_values(item_data: dict) -> dict:
         # Generate random values for weight, durability, and rarity
         item_data['weight'] = random.uniform(0.1, 10.0)
         item_data['durability'] = random.uniform(0.1, 0.9)
@@ -12,3 +15,12 @@ class MagicItemService:
 
         # Call the repository to create the item in the database
         return MagicItemRepository.create_item(item_data)
+
+    @staticmethod
+    def get_all_items() -> list[dict[Any, Any]]:
+        # Call the repository to get all items from the database
+        return MagicItemRepository.get_all_items()
+
+# create something to convert rarity to Common Rare etc.
+# create something like test_item to lower the durability after each test.
+# create something that when the durability reaches below 0 to be "destroyed"
