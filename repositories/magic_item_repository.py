39fbfe6562300bc3fe24cc5e0492c60b9914fdbd
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-04 16:10:04 +0300

    add get_all_items method

--- repositories/magic_item_repository.py
+++ repositories/magic_item_repository.py
@@ -1,3 +1,5 @@
+from typing import List, Dict, Any
+
 from db import get_connection
 
 
@@ -27,4 +29,25 @@ class MagicItemRepository:
             return {**item_data, "id": item_id}
         except Exception as e:
             conn.rollback()
+            raise e
+
+    @staticmethod
+    def get_all_items() -> list[dict[Any, Any]]:
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM magic_items")
+            items = cursor.fetchall()
+
+            cursor.close()
+            conn.close()
+
+            # Assuming your table has columns in this specific order
+            column_names = ['id', 'name', 'description', 'level', 'type', 'category', 'rarity_value', 'weight', 'value',
+                            'durability']
+            all_items = [dict(zip(column_names, item)) for item in items]
+
+            return all_items
+        except Exception as e:
             raise e
